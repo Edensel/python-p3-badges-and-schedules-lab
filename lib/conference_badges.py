b841aa83c18c7359f25bccb530f98dc0ec5129a1
Densel <densel.esekon@moringaschool.com>
2024-01-21 19:11:48 +0300

    Completed

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,27 @@
 def badge_maker(name):
-    return None
+    name_of_person = f'Hello, my name is {name}.'
+    return name_of_person
 
 def batch_badge_creator(names):
-    return None
+    list_name = []
+    for name in names:
+        list_name.append(f'Hello, my name is {name}.')
+       
+    return list_name
 
 def assign_rooms(names):
-    return None
+    list_name =[]
+    rooms = [1,2,3,4,5,6,7,8]
+    for i in range(len(names)):
+        if i < len(rooms):
+              list_name.append(f"Hello, {names[i]}! You'll be assigned to room {rooms[i]}!")
+        else:
+             return "There are no enough rooms for the users"
+    return list_name
+              
 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+        print(badge)
+    for assignment in assign_rooms(names):
+        print(assignment)
